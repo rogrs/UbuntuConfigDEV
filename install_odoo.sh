b6182be9cd5fba456bbf68dfcadbcf27eda67229
Rogerio Ribeiro <rogerio.rs@gmail.com>
2016-11-16 01:20:42 -0200

    Ajuste no script

--- install_odoo.sh
+++ install_odoo.sh
@@ -8,7 +8,7 @@ sudo ufw enable
 
      
 
-sudo apt-get install postgresql -y
+sudo apt-get install postgresql pgadmin3 -y
 
 sudo apt-get install subversion git bzr bzrtools python-pip python-all-dev python-dev python-setuptools libxml2-dev libxslt1-dev libevent-dev libsasl2-dev libldap2-dev pkg-config libtiff5-dev libjpeg8-dev libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev liblcms2-utils libwebp-dev tcl8.6-dev tk8.6-dev python-tk libyaml-dev fontconfig -y
 
@@ -22,3 +22,5 @@ sudo chmod -R 777 "/var/log/odoo"
 sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch 9.0 --single-branch .
 
 sudo su - postgres
+
+
