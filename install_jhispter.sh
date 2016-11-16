89ba2c60c5f1438ae5435256f9de3dcf9ecb67d2
Rogerio Ribeiro <rogerio.rs@gmail.com>
2016-11-15 22:47:02 -0200

    Script pra realizar instalação do Jhispter

---
+++ install_jhispter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo npm install -g yo
+sudo npm install -g bower
+sudo npm install -g gulp-cli
+sudo npm install -g generator-jhipster
+sudo npm install -g ionic yo bower gulp
+sudo npm install -g generator-jhipster-ionic
+
+sudo npm install -g npm-check-updates
+
