8079c95a9ed372d24c1bc04f7fbc9b99130a6285
rogerio.ribeiro <rogerio.ribeiro@m4u.com.br>
2017-07-05 18:17:00 -0300

    Ajuste no script para execução automatica

--- install.sh
+++ install.sh
@@ -1,66 +1,53 @@
 #!/bin/bash
-
-
-sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer -y
-sudo apt-get install gnome-panel -y
-sudo apt-get install pngquant -y
-sudo apt-get install libjpeg-dev -y
-
 shome=`dirname $0`
-sudo mkdir  -p "$shome/dev/maven"
-sudo chmod -R 777 "$shome/dev/maven"
+smaven='3.5.0'
 
-cd "$shome/dev/maven"
+sudo apt-get install git -y &&
+sudo apt-get install python-software-properties -y &&
+sudo add-apt-repository ppa:webupd8team/java &&
+sudo apt-get update &&
+sudo apt-get install gnome-panel -y &&
+sudo apt-get install pngquant -y &&
+sudo apt-get install libjpeg-dev -y &&
+sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y &&
+sudo apt-get install oracle-java8-installer -y &&
+sudo apt-get autoremove -y &&
+sudo apt-get autoclean -y &&
+sudo mkdir  -p "$shome/dev/maven" &&
+sudo chmod -R 777 "$shome/dev/maven" &&
+cd "$shome/dev/maven" &&
 
- 
-wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-tar -xzvf apache-maven-3.3.9-bin.tar.gz
-rm apache-maven-3.3.9-bin.tar.gz
-cd ..
-cd ..
+wget http://ftp.unicamp.br/pub/apache/maven/maven-3/${smaven}/binaries/apache-maven-${smaven}-bin.tar.gz &&
+tar -xzvf apache-maven-${smaven}-bin.tar.gz &&
+rm apache-maven-${smaven}-bin.tar.gz &&
+cd .. &&
+cd .. &&
 
-sudo sed -i '$ a source ~/profile' ~/.bashrc
-cat ~/.bashrc
-source ~/.bashrc
+sudo sed -i '$ a source ~/profile' ~/.bashrc &&
+cat ~/.bashrc &&
+source ~/.bashrc &&
 
-
-java -version
+java -version &&
 mvn –version
 
-
-sudo apt-get install git -y
-
-sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
-
 #Install
-#yum -y install nodejs
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
-
-sudo apt-get install build-essential
-sudo apt-get install build-essential libssl-dev
+sudo apt-get update &&
+sudo apt-get install nodejs &&
+sudo apt-get install npm &&
+sudo apt-get install build-essential &&
+sudo apt-get install build-essential libssl-dev &&
 
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs &&
+sudo apt-get autoremove -y &&
+sudo npm install -g forever &&
+sudo npm install -g forever-service &&
+sudo apt-get install libsnmp-dev -y &&
+sudo apt-get install libjpeg62 libjpeg62-dev -y &&
+sudo apt-get install snmp libsnmp-dev -y &&
+gcc -v &&
+make -v 
 
-sudo apt-get autoremove -y
-
-
-sudo npm install -g forever
-sudo npm install -g forever-service
-
-
-#sudo forever-service install imovel -s consulta-imoveis-1.0.0-SNAPSHOT.jar -f " -c 'java -Xms1024m -Xmx1024m -jar'"
-
-gcc -v
-make -v
-sudo apt-get install libsnmp-dev -y
-sudo apt-get install libjpeg62 libjpeg62-dev -y
-sudo apt-get install snmp libsnmp-dev -y
 
 
 
