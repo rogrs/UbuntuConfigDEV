246d6ac7d973823f14e0bd0fb00ca0cefbc4fe28
Rogerio Ribeiro <rogerio.rs@gmail.com>
2017-08-28 23:15:15 -0300

    add scripts para config mysql, mercurial e extras

--- install_extras.sh
+++ install_extras.sh
@@ -8,7 +8,20 @@ sudo apt-get install smplayer smplayer-themes smplayer-skins &&
 
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/osmc/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/osmc-installer.list" &&
 sudo apt-get update &&
-sudo apt-get install osmc-installer 
+sudo apt-get install osmc-installer -y &&
+
+
+sudo apt-get install iasl -y &&
+sudo apt-get install bison -y &&
+sudo apt-get install flex-old -y &&
+sudo apt-get purge laptop-mode-tools -y &&
+#sudo add-apt-repository ppa:linrunner/tlp &&
+#sudo add-apt-repository ppa:linrunner/tlp --remove &&
+sudo apt-get update &&
+sudo apt-get install tlp -y &&
+sudo tlp start &&
+
+sudo dpkg-reconfigure -plow unattended-upgrades &&
 
 
 
--- install_mysql.sh
+++ install_mysql.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y
+sudo apt-get install mysql-server -y &&
+sudo apt-get install phpmyadmin php-mbstring php-gettext -y
 
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
 
-sudo apt-get install phpmyadmin -y
