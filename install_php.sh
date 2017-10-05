69a59ba489320935729c1fc72f5ab2defa566f59
rogerio.ribeiro <rogerio.ribeiro@m4u.com.br>
2017-10-05 12:33:37 -0300

    Add script para instalar o php e configurar

---
+++ install_php.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get install python-software-properties -y &&
+sudo add-apt-repository ppa:ondrej/php &&
+sudo apt-get update &&
+sudo apt-get purge php5-fpm -y &&
+sudo apt-get install php7.0 php7.0-fpm -y &&
+sudo apt-get install libapache2-mod-php7.0 -y &&
+sudo apt-get install php7.0-mysql -y &&
+sudo apt-get --purge autoremove -y &&
+sudo apt-get install phpmyadmin php-mbstring php-gettext -y &&
+sudo apt-get install php7.0-mbstring -y &&
+sudo apt-get install php7.0-fpm php7.0-mysql php7.0-common php7.0-gd php7.0-json php7.0-cli php7.0-curl libapache2-mod-php7.0 -y &&
+sudo a2enmod php7.0 &&
+sudo systemctl restart apache2 &&
+clear &&
+php --version &&
+
+
+
