dc3fc525287a272e36e03ec9f53fea6a053c6517
Rogerio Ribeiro <rogerio.rs@gmail.com>
2017-11-02 22:31:40 -0200

    script para instalar o go lang

--- install_golang.sh
+++ install_golang.sh
@@ -4,4 +4,9 @@ sudo apt-get install golang-go -y &&
 sudo add-apt-repository ppa:gophers/archive &&
 sudo apt update &&
 sudo apt-get install golang-1.9-go -y &&
+snap install --classic go &&
+sudo apt-get install golang git mercurial -y &&
 go version
+
+
+
