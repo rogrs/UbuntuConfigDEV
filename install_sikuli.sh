be0998d849f09d629cb9b346a8135cf2f455a313
Rogerio Ribeiro <rogerio.rs@gmail.com>
2017-11-02 22:31:15 -0200

    Adicionando script sikuli

---
+++ install_sikuli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install software-properties-common -y &&
+sudo add-apt-repository ppa:george-edison55/cmake-3.x &&
+sudo apt-get update &&
+sudo apt-get install cmake &&
+sudo apt-get install build-essential -y &&
+sudo apt-get install checkinstall -y &&
+sudo apt install libopencv-dev -y &&
+sudo apt install tesseract-ocr -y
