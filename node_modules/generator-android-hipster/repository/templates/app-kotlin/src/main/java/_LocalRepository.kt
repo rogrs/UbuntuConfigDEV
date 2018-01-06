729feee36211b0053840e2f1e9a70a84a5646ad6
Rogério Ribeiro <rogerio.rs@gmail.com>
2018-01-06 07:06:41 -0200

    removido arquivos lixo de instalações

--- node_modules/generator-android-hipster/repository/templates/app-kotlin/src/main/java/_LocalRepository.kt
+++
@@ -1,8 +0,0 @@
-package <%= appPackage %>.domain.repository.<%= repositoryPackageName %>
-
-import javax.inject.Inject
-
-class <%= repositoryName %>LocalRepository @Inject constructor() {
-
-
-}
--- node_modules/generator-android-hipster/usecase/templates/app-kotlin/src/main/java/usecase/_UseCaseInterface.kt
+++
@@ -1,9 +0,0 @@
-package <%= appPackage %>.domain.usecases.<%= useCasePackageName %>;
-
-import rx.Observable
-
-interface <%= useCaseName %>UseCase {
-
-    operator fun invoke() : Observable<Any>
-
-}
