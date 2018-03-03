de7daaa7033eaf20dc7d8d6166591702ee9fb7f2
Rogério Ribeiro <rogerio.rs@gmail.com>
2018-03-02 23:29:25 -0300

    Instalação do samba no ubuntu

---
+++ install_samba.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#https://help.ubuntu.com/community/How%20to%20Create%20a%20Network%20Share%20Via%20Samba%20Via%20CLI%20%28Command-line%20interface/Linux%20Terminal%29%20-%20Uncomplicated%2C%20Simple%20and%20Brief%20Way%21
+sudo apt-get update
+sudo apt-get install samba -y &&
+sudo apt-get install smbclient -y
