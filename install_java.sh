6550e81ea0f7ed34f1075b52f9abc3cb4fd3f0e7
Rogério Ribeiro <rogerio.rs@gmail.com>
2018-03-15 23:06:08 -0300

    Ajuste para instalar o java 8 java 9 comentado

--- install_java.sh
+++ install_java.sh
@@ -3,5 +3,6 @@
 
 echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java &&
 sudo apt-get update &&
-echo -ne '\n' | sudo apt-get install oracle-java9-installer -y &&
+echo -ne '\n' | sudo apt-get install oracle-java8-installer -y &&
+#echo -ne '\n' | sudo apt-get install oracle-java9-installer -y &&
 java -version
