0d2c38d2c90f143f72c51a239c4929354f27fb6a
IVENS VIANNA DA ROCHA <ivrocha@br.ibm.com>
2018-05-03 17:05:28 -0300

    novo teste de config

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#ip link add link eth0 address 28:D2:44:0D:F6:0C eth0.1 type macvlan &&
-#ifconfig eth0.1 up 
-ifconfig eth0.1
+ip link add link eth0 address 28:D2:44:0D:F6:0C eth0.1 type macvlan &&
+ifconfig eth0.1 up &&
+ifconfig eth0.1 
 
 
 BRANCH=git branch
