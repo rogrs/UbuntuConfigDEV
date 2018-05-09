c1972ea7dd8aa35e25c9d6ce0bdfb3ae2016aaac
rogrs <rogerio.rs@gmail.com>
2018-05-09 17:40:52 +0000

    novo ajuste

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#ip link add link eth0 address 28:D2:44:0D:F6:0C eth0.1 type macvlan &&
-#ifconfig eth0.1 up &&
-#ifconfig eth0.1 
-
-ifconfig
+export BRANCH = bash -c 'echo $TRAVIS_BRANCH'
 echo "Get TRAVIS_BUILD_DIR  ${TRAVIS_BUILD_DIR}"
 echo "Get TRAVIS_BRANCH ${TRAVIS_BRANCH}"
-echo "Executando URI (GET): " ${SERVER}
-echo "Executando URI (GET): " ${USER}
+echo "Executando URI (GET):  ${SERVER} "
+echo "Executando URI (GET):  ${USER}"
+
+
+sshpass --help
