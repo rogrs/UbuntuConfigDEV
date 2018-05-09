86b3caf57c3a5a1d8afbb9a01a22a46c4004f748
rogrs <rogerio.rs@gmail.com>
2018-05-09 17:47:57 +0000

    ajuste

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-export BRANCH = bash -c 'echo $TRAVIS_BRANCH'
-echo "Get TRAVIS_BUILD_DIR  ${TRAVIS_BUILD_DIR}"
-echo "Get TRAVIS_BRANCH ${TRAVIS_BRANCH}"
-echo "Executando URI (GET):  ${SERVER} "
-echo "Executando URI (GET):  ${USER}"
-
-
-sshpass --help
+pwd
+ls -l
+sshpass -h
