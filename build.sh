05838439544ae787f3dd1649acb3035fdcb75f5d
IVENS VIANNA DA ROCHA <ivrocha@br.ibm.com>
2018-05-09 11:05:45 -0300

    test values travis

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 #ifconfig eth0.1 
 
 ifconfig
-
+echo "Get TRAVIS_BUILD_DIR  ${TRAVIS_BUILD_DIR}"
+echo "Get TRAVIS_BRANCH ${TRAVIS_BRANCH}"
 echo "Executando URI (GET): " ${SERVER}
 echo "Executando URI (GET): " ${USER}
