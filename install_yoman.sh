85576dbb948b3bdd116c3635089cf331425d321f
rogerio ribeiro <rogerio.rs@gmail.com>
2018-05-26 23:47:01 -0300

    Script para instalar dependencias yo man

---
+++ install_yoman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo npm install -g yo &&
+sudo npm install -g generator-webapp &&
+sudo npm install --global yo gulp-cli bower generator-webapp &&
+sudo npm install --global yo generator-webapp
+
+#update
+sudo npm i -g bower
