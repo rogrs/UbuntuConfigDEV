5a7ea3b74ddd069cb75d2989439afe29904ffa7c
rogerio ribeiro <rogerio.rs@gmail.com>
2018-05-26 23:46:32 -0300

    Script para instalar o cli tools

---
+++ install_cli_tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install powerman -y &&
+sudo npm install -g composer-cli &&
+sudo npm install -g composer-rest-server &&
+sudo npm install -g generator-hyperledger-composer &&
+sudo npm install -g yo
